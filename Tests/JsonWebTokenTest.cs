f2e6743b8a3455774afb31bd78cab64a5a6a76b1
Peter Klaesson <peter.klaesson@stratsys.se>
2017-03-07 15:48:59 +0100

    Added test for JWT/JWE

---
+++ Tests/JsonWebTokenTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using jwt;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class JsonWebTokenTest
+    {
+        [Test]
+        public void TestEncryptDecrypt()
+        {
+            var jwt = new JsonWebToken();
+            var encrypted = jwt.Encrypt("hemligt", "lösenord");
+            var decrypted = jwt.Decrypt<string>(encrypted, "lösenord");
+
+            Assert.That(decrypted, Is.EqualTo("hemligt"));
+        }
+
+        [Test]
+        public void EncryptDecryptUrlParameters()
+        {
+            var jwt = new JsonWebToken();
+            var utcTime = new DateTime(2017, 01, 01, 8, 30, 20).ToUniversalTime();
+            var parameters = new Parameters
+            {
+                UserName = "user",
+                TimeStamp = utcTime
+            };
+            var encrypted = jwt.Encrypt(parameters, "Nr8t98k6Se9i11t7hfhfhfghjgfhjghkrt7etyjrtyjtyjdtyjhdtyjfgyjgfyjy");
+            var decrypted = jwt.Decrypt<Parameters>(encrypted, "Nr8t98k6Se9i11t7hfhfhfghjgfhjghkrt7etyjrtyjtyjdtyjhdtyjfgyjgfyjy");
+
+            Assert.That(decrypted.UserName, Is.EqualTo("user"));
+            Assert.That(decrypted.TimeStamp, Is.EqualTo(utcTime));
+        }
+
+        private class Parameters
+        {
+            public DateTime TimeStamp { get; set; }
+            public string UserName { get; set; }
+        }
+    }
+}
---
+++ jwt/JsonWebToken.cs
@@ -0,0 +1,32 @@
+﻿using Jose;
+
+namespace jwt
+{
+    public class JsonWebToken
+    {
+        private JweAlgorithm _algorithm = JweAlgorithm.PBES2_HS512_A256KW;
+        private JweEncryption _encryption = JweEncryption.A256CBC_HS512;
+
+        /// <summary>
+        /// Encrypts payload using a shared secret
+        /// </summary>
+        /// <param name="payload"></param>
+        /// <param name="secret"></param>
+        /// <returns>The encrypted result as base64Url encoded token</returns>
+        public string Encrypt(object payload, string secret)
+        {
+            return JWT.Encode(payload, secret, _algorithm, _encryption);
+        }
+
+        /// <summary>
+        /// Decrypts the jwt token using a shared secret
+        /// </summary>
+        /// <param name="encrypted"></param>
+        /// <param name="secret"></param>
+        /// <returns>payload of jwt token</returns>
+        public T Decrypt<T>(string encrypted, string secret)
+        {
+            return JWT.Decode<T>(encrypted, secret, _algorithm, _encryption);
+        }
+    }
+}
